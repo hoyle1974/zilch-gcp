411b0b2c94691ddc7eaad64b2b2b506870405571
Zilch Developer <hoyle.hoyle@gmail.com>
2026-06-19 15:16:24 -0700

    fix: use ANSI-C quoting for color variable definitions
    Change color variables from single quotes to $'...' ANSI-C quoting syntax
    to properly interpret escape sequences when expanded in prompts.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-# Color definitions
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-BOLD='\033[1m'
-NC='\033[0m'
+# Color definitions (ANSI-C quoting for proper interpretation)
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+YELLOW=$'\033[1;33m'
+BLUE=$'\033[0;34m'
+CYAN=$'\033[0;36m'
+BOLD=$'\033[1m'
+NC=$'\033[0m'
 
 clear
 echo -e "${BOLD}${CYAN}Zilch GCP Infrastructure Deployment${NC}"
